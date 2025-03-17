7af78af2c6f2e4f2a21b398cd6a3c240d2a8c7a5
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-03-16 20:25:51 -0400

    Merge flettest to src/

--- src/main.py
+++ src/main.py
@@ -1,24 +1,50 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
+def main(page):
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    page.adaptive = True
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+    page.appbar = ft.AppBar(
+        leading=ft.TextButton("New", style=ft.ButtonStyle(padding=0)),
+        title=ft.Text("Adaptive AppBar"),
+        actions=[
+            ft.IconButton(ft.cupertino_icons.ADD, style=ft.ButtonStyle(padding=0))
+        ],
+        bgcolor=ft.Colors.with_opacity(0.04, ft.CupertinoColors.SYSTEM_BACKGROUND),
     )
+
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Your List",
+            ),
+            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explore"),
+            ft.NavigationBarDestination(icon=ft.Icons.FEED_ROUNDED, label="Recent News"),
+        ],
+        border=ft.Border(
+            top=ft.BorderSide(color=ft.CupertinoColors.SYSTEM_GREY2, width=0)
+        ),
+    )
+
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
+            ft.Column(
+                [
+                    ft.Checkbox(value=False, label="Dark Mode"),
+                    ft.Text("First field:"),
+                    ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
+                    ft.Text("Second field:"),
+                    ft.TextField(keyboard_type=ft.KeyboardType.TEXT),
+                    ft.Switch(label="A switch"),
+                    ft.FilledButton(content=ft.Text("Adaptive button")),
+                    ft.Text("Text line 1"),
+                    ft.Text("Text line 2"),
+                    ft.Text("Text line 3"),
+                ]
+            )
         )
     )
 
